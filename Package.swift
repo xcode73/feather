f8a3bb1e25e722012d03d862b01d0e1d312d2061
xcode73 <xcode.uln@gmail.com>
2024-02-23 22:34:46 +0400

    swift-tools-version:5.9

--- Package.swift
+++ Package.swift
@@ -4,20 +4,19 @@ import PackageDescription
 let package = Package(
     name: "app",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_15)
     ],
     products: [
         .executable(name: "App", targets: ["App"])
     ],
     dependencies: [
-        .package(url: "https://github.com/xcode73/feather-core", branch: "test-dev"),
-        .package(url: "https://github.com/xcode73/user-module", branch: "test-dev"),
-        .package(url: "https://github.com/xcode73/web-module", branch: "test-dev"),
-        .package(url: "https://github.com/xcode73/redirect-module", branch: "test-dev"),
-
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.6.0"),
-        .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.3.2"),
-        .package(url: "https://github.com/binarybirds/mail-aws-driver", from: "0.0.2"),
+        .package(url: "https://github.com/xcode73/feather-core.git", branch: "test-dev"),
+        .package(url: "https://github.com/xcode73/user-module.git", branch: "test-dev"),
+        .package(url: "https://github.com/xcode73/web-module.git", branch: "test-dev"),
+        .package(url: "https://github.com/xcode73/redirect-module.git", branch: "test-dev"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.6.0"),
+        .package(url: "https://github.com/binarybirds/liquid-local-driver.git", from: "1.3.2"),
+        .package(url: "https://github.com/binarybirds/mail-aws-driver.git", from: "0.0.2")
     ],
     targets: [
         .executableTarget(name: "App", dependencies: [
@@ -25,10 +24,9 @@ let package = Package(
             .product(name: "UserModule", package: "user-module"),
             .product(name: "WebModule", package: "web-module"),
             .product(name: "RedirectModule", package: "redirect-module"),
-            
             .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
             .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
-            .product(name: "MailAwsDriver", package: "mail-aws-driver"),
+            .product(name: "MailAwsDriver", package: "mail-aws-driver")
         ]),
     ],
     swiftLanguageVersions: [.v5]
