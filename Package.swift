9c0fc0e1fccf8fd0c54f1e8f44a76325ec38994a
Nikolai Eremenko <nikolai.dev@icloud.com>
2024-06-08 14:10:51 +0400

    build: update dependencies

--- Package.swift
+++ Package.swift
@@ -10,19 +10,13 @@ let package = Package(
         .executable(name: "App", targets: ["App"])
     ],
     dependencies: [
-        //local
-        .package(path: "../feather-core"),
-        .package(path: "../user-module"),
-        .package(path: "../web-module"),
-        .package(path: "../redirect-module"),
-        
-//        .package(url: "https://github.com/xcode73/feather-core.git", branch: "test-dev"),
-//        .package(url: "https://github.com/xcode73/user-module.git", branch: "test-dev"),
-//        .package(url: "https://github.com/xcode73/web-module.git", branch: "test-dev"),
-//        .package(url: "https://github.com/xcode73/redirect-module.git", branch: "test-dev"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.6.0"),
-        .package(url: "https://github.com/binarybirds/liquid-local-driver.git", from: "1.3.2"),
-        .package(url: "https://github.com/binarybirds/mail-aws-driver.git", from: "0.0.1")
+        .package(url: "https://github.com/xcode73/feather-core", branch: "test-dev"),
+        .package(url: "https://github.com/xcode73/user-module", branch: "test-dev"),
+        .package(url: "https://github.com/xcode73/web-module", branch: "test-dev"),
+        .package(url: "https://github.com/xcode73/redirect-module", branch: "test-dev"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.6.0"),
+        .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.3.0"),
+        .package(url: "https://github.com/binarybirds/mail-aws-driver", from: "0.0.1")
     ],
     targets: [
         .executableTarget(name: "App", dependencies: [
